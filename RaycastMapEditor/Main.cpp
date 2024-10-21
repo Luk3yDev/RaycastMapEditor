29bd1d7e0fc2c7bbc6ff4063b64cdf20c3662bbc
Luk3yDev <lukemadsen501@gmail.com>
2024-10-21 14:04:35 +1300

    Why not

--- RaycastMapEditor/Main.cpp
+++ RaycastMapEditor/Main.cpp
@@ -9,7 +9,7 @@ const int tileSize = 32;
 const int mapWidth = 25;
 const int mapHeight = 25;
 
-const int TILE_TYPES[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+const int TILE_TYPES[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
 const int numTileTypes = sizeof(TILE_TYPES) / sizeof(TILE_TYPES[0]);
 
 int currentTileIndex = 0;
